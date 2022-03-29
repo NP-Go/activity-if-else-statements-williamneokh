466f48db10aa85fbfc4af423587f67a4ef009432
williamneo <williamneokh@gmail.com>
2022-03-29 22:50:04 +0800

    Done activity #3 if else statements

--- go.mod
+++
@@ -1,3 +0,0 @@
-module GoBaActivity_IfElseStatement3
-
-go 1.18
--- ifElseIfStatement.go
+++ ifElseIfStatement.go
@@ -8,9 +8,13 @@ import (
 func evaluateValue(value int64) string {
 	replyMessage := ""
 	//insert your code here
-
+	if value%5 == 0 && value%6 == 0 {
+		replyMessage = "The value is divisible by 5 and 6"
+	} else {
+		replyMessage = "The value is NOT divisible by 5 and 6"
+	}
 	//Do not remove these lines
-	fmt.Println((replyMessage))
+	fmt.Println(replyMessage)
 	return replyMessage
 
 }
@@ -18,7 +22,7 @@ func evaluateValue(value int64) string {
 func main() {
 	var compareValue string
 	fmt.Println("Enter an integer: ")
-	fmt.Scanln(&compareValue)
+	_, _ = fmt.Scanln(&compareValue)
 
 	//conversion of value
 	valueInt, _ := strconv.ParseInt(compareValue, 10, 0)
